43d5da8ef924d8f9566754d5afb137a2904eaf36
heroes618 <heroes618@hotmail.com>
2015-04-22 09:17:09 -0700

    video 8

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,11 @@
 <?php 
-$mysqli = new mysqli('localhost', 'root', 'root',  'todo');
+$mysqli = new mysqli('localhost', 'root', 'root',  'todo1');
 if($mysqli->connect_error){
 	die('Connect Error(' . $mysqli->connect_errno . ')'
 		. $mysqli->connect_error);
 }
 else{
-	echo "Connection made";
+	//echo "Connection made";
 
 }
 $mysqli->close();
